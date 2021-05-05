34bdd59cbc59e5c10252fafbefb91cf291c088f8
Vladimir <vovav2005@yandex.ru>
2021-05-06 00:47:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Как поднять API на локальной машине
+1. Скачать репозиторий https://github.com/Vladimir-Vorobev/yandex_backend_school_exam
+2. Установить PyCharm Community Edition с официального сайта: https://www.jetbrains.com/ru-ru/pycharm/download/
+3. Открыть папку проекта с помощью PyCharm
+4. В PyCharm перейти в File -> Settings -> Project -> Python Interpreter
+5. Выбрать Base Interpreter или настроить свой (например, Anaconda) и нажать OK
+6. Перейти в Terminal и установить все нужные зависимости: pip install -r requirements.txt
+7. Запустить bot.py с помощью Shift+F10 или Run -> Run... -> main
+8. Перейти в бот @TestPushkinMuseumBot
---
+++ bot.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+
+# Объект бота
+bot = Bot(token="1770602296:AAGUQCL-5MrSoNf4ByyhAxakYLnww7c0-zA")
+# Диспетчер для бота
+dp = Dispatcher(bot)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# импортируем класс ответов
+from messages import mes
+
+
+# Хэндлер на команду /test1
+@dp.message_handler(commands=['start', 'restart'])
+async def cmd_test1(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Квест', 'Прогулка']
+    keyboard.add(*buttons)
+    user = message.from_user.first_name
+    if user:
+        if message.from_user.last_name:
+            user += ' ' + message.from_user.last_name
+        await message.answer("{}, т{}".format(user, mes.welcome()))
+    else:
+        await message.answer("Т{}".format(mes.welcome()))
+    await message.answer("Вот и первая развилка 🗺")
+    await message.answer(mes.describe_quest_walk(), reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Квест"))
+async def with_puree(message: types.Message):
+    await message.reply(mes.start_quest(), reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(mes.restart())
+
+
+@dp.message_handler(Text(equals="Прогулка"))
+async def without_puree(message: types.Message):
+    with open("museum.jpg", "rb") as file:
+        data = file.read()
+    await message.reply(mes.start_walk(), reply_markup=types.ReplyKeyboardRemove())
+    await bot.send_photo(message.from_user.id, photo=data)
+    await message.answer(mes.restart())
+
+
+if __name__ == "__main__":
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
